95aee3466d0eef228c0db907b552ac2ef84b7f54
JustLuaDeveloper <stasmaks5423@gmail.com>
2026-02-19 16:01:06 +0300

    Update func.js

--- func.js
+++ func.js
@@ -1,1 +1,24 @@
+function showTab(id, element) {
+    document.querySelectorAll('.section').forEach(sec => {
+        sec.classList.remove('active');
+    });
+
+    document.getElementById(id).classList.add('active');
+
+    document.querySelectorAll('.nav-item').forEach(item => {
+        item.classList.remove('active');
+    });
+
+    element.classList.add('active');
+}
+
+function searchObfuscator() {
+    const input = document.getElementById("searchInput").value.toLowerCase();
+    const items = document.querySelectorAll(".obf-item");
+
+    items.forEach(item => {
+        const title = item.querySelector("h2").innerText.toLowerCase();
+        item.style.display = title.includes(input) ? "block" : "none";
+    });
+}
 
